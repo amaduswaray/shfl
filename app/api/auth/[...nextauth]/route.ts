3f641f4ba539724d8f93be6457ff00d0f546bcd8
Amadu Swaray <amaduswaray06@gmail.com>
2024-10-05 15:42:55 +0200

    Updated Next-auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,32 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
+import { type NextAuthOptions } from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
 
-// TODO: Change provider to spotify
-const authOptions = {
+const options: NextAuthOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GAUTH_CLIENT_ID!,
-      clientSecret: process.env.GAUTH_CLIENT_SECRET!,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    SpotifyProvider({
+      authorization:
+        "https://accounts.spotify.com/authorize?client_id=4e1814917bcd4433976396284bcd5df4&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-modify-public%20playlist-modify-private",
+      clientId: process.env.SPOTIFY_CLIENT_ID || "",
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET!,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.access_token = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        token,
+      };
+    },
+  },
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
