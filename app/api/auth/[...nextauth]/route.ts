d157a471a5014ba302dfe7dc45f63a964de36b99
Amadu Swaray <amaduswaray06@gmail.com>
2024-10-05 16:32:50 +0200

    Spotify auth init

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,11 @@
 import NextAuth from "next-auth/next";
-import axios from "axios";
 import { type NextAuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { JWT } from "next-auth/jwt";
-const LOGIN_URL =
-  "https://accounts.spotify.com/authorize?client_id=4e1814917bcd4433976396284bcd5df4&response_type=code&redirect_uri=http://localhost:3000/api/auth/callback/spotify&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-modify-public%20playlist-modify-private";
-
-/* const refreshAccessToken = async (token: JWT) {
-  const params = new URLSearchParams();
-  params.append("grant_type", "refresh_token");
-  params.append("refresh_token", token.refreshToken);
-  
-  const response = axios.post(LOGIN_URL, null, {headers})
-
-} */
 
 const options: NextAuthOptions = {
   providers: [
     SpotifyProvider({
-      authorization: LOGIN_URL,
+      authorization: process.env.SPOTIFY_LOGIN_URL!,
       clientId: process.env.SPOTIFY_CLIENT_ID || "",
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
     }),
@@ -32,11 +19,6 @@ const options: NextAuthOptions = {
         return token;
       }
 
-      // Access token has not expired
-      // if (Date.now() < token.accessTokenExpires * 1000) return token;
-
-      // Access token has expired
-      // return refreshAccessToken(token);
       return token;
     },
     async session({ session, token }) {
--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 // TODO: Add all other auth variables needed for the context
+// useEffect for fetching data, as well as in game stats
+// Spotify is most played songs
+// In games stats are from the
 function AuthProvider({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
 }
