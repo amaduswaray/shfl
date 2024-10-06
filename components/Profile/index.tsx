f00cc0163da32e50396d711c7c0cd257c3b28477
Amadu Swaray <amaduswaray06@gmail.com>
2024-10-06 21:34:06 +0200

    components for profile

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { SpotifyContext } from "@/context/SpotifyContext";
 import Image from "next/image";
+import Cross from "@icons/cross.svg";
 
 const Profile = () => {
   const spotifyData = useContext(SpotifyContext);
@@ -13,9 +14,9 @@ const Profile = () => {
   };
   return (
     <div className="h-full w-full flex flex-col">
-      <div className="flex flex-row justify-between items-center w-full max-w-lg h-20 top-0 fixed nav px-5">
-        <div></div>
-        <p>x</p>
+      <div className="flex flex-row justify-between items-center w-full max-w-lg h-20 top-0 fixed nav px-10">
+        <div className="spacer"></div>
+        <Cross />
       </div>
       <div className="w-full h-5/6 flex flex-col justify-center items-center gap-2 mt-20">
         <div className="w-full flex flex-col justify-center items-center gap-2">
@@ -30,9 +31,67 @@ const Profile = () => {
             {profileData.name}
           </p>
         </div>
-        <div className="stats container flex flex-row justify-between items-center gap-5">
-          <div className="top songs"></div>
-          <div className="game stats flex flex-col justify-start gap-5 items-center"></div>
+
+        <div className="flex flex-row justify-center items-center gap-5 h-4/6 w-full p-10">
+          <div className="w-1/2 h-full flex flex-col justify-start items-center gap-3">
+            <h2 className="text-shfl-white font-medium text-md">Top Songs</h2>
+            <div className="bg-shfl-gray h-5/6 w-full p-3 rounded-sm flex flex-col gap-3">
+              <div className="w-full flex flex-row gap-3 justify-between">
+                <h2 className="text-shfl-pink font-medium text-md">1</h2>
+                <div className="flex flex-col overflow-hidden">
+                  <p className="text-shfl-white text-xs font-base truncate hover:text-clip">
+                    Title That is very long
+                  </p>
+                  <p className="text-shfl-pink text-xs font-extralight">
+                    Artist
+                  </p>
+                </div>
+              </div>
+              {/*<div className="w-full flex flex-row gap-3 justify-between">
+                <h2 className="text-shfl-white font-medium text-md">1</h2>
+                <div className="flex flex-col overflow-hidden">
+                  <p className="text-shfl-white text-xs font-base truncate hover:text-clip">
+                    Title That is very long
+                  </p>
+                  <p className="text-shfl-pink text-xs font-extralight">
+                    Artist
+                  </p>
+                </div>
+              </div>*/}
+            </div>
+          </div>
+          <div className="flex flex-col justify-start gap-10 items-center w-1/2 h-full">
+            <div className="flex flex-col justify-start items-center gap-3 w-full px-2">
+              <h2 className="text-shfl-white font-medium text-md">
+                Times Played
+              </h2>
+              <div className="bg-shfl-gray w-full p-3 rounded-sm flex justify-center items-center">
+                <p className="text-shfl-pink font-medium text-lg">
+                  {profileData.gamesPlayed}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col justify-start items-center gap-3 w-full px-2">
+              <h2 className="text-shfl-white font-medium text-md">
+                Shots Given
+              </h2>
+              <div className="bg-shfl-gray w-full p-3 rounded-sm flex justify-center items-center">
+                <p className="text-shfl-pink font-medium text-lg">
+                  {profileData.shotsGiven}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col justify-start items-center gap-3 w-full px-2">
+              <h2 className="text-shfl-white font-medium text-md">
+                Shots Taken
+              </h2>
+              <div className="bg-shfl-gray w-full p-3 rounded-sm flex justify-center items-center">
+                <p className="text-shfl-pink font-medium text-lg">
+                  {profileData.shotsTaken}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
