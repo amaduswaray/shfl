a62c2167f68c2ba7ada00f2a13b67d24dfaa09c4
Amadu Swaray <amaduswaray06@gmail.com>
2024-10-09 19:26:49 +0200

    Host game done, without creating playlist

--- app/api/game/create/route.ts
+++ app/api/game/create/route.ts
@@ -56,6 +56,7 @@ export async function POST(req: Request) {
             songCount: settings.songCount,
             songPeriod: settings.songPeriod,
             hostId: hostUser.id,
+            songIndexes: songIndexes,
           },
         });
 
