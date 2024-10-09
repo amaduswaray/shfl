279037376ae9b809e7acb0221db919f3f2bc1e11
Amadu Swaray <amaduswaray06@gmail.com>
2024-10-09 18:45:39 +0200

    New migrations

--- app/api/game/create/route.ts
+++ app/api/game/create/route.ts
@@ -1,4 +1,5 @@
 import { GameSettings } from "@/interfaces/game";
+import { prisma } from "@/lib/prisma";
 
 const GenerateCode = () => {
   const elements = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -11,6 +12,15 @@ export async function POST(req: Request) {
   const body: GameSettings = await req.json();
   const code = GenerateCode();
   const settings: GameSettings = { ...body, sessionId: code };
+  const newGame = await prisma.game.create({
+    data: {
+      sessionId: settings.sessionId,
+      title: settings.title,
+      status: "LOBBY",
+      songCount: settings.songCount,
+      songPeriod: settings.songPeriod,
+    },
+  });
   console.log(settings);
   return Response.json({ sucess: true });
 }
